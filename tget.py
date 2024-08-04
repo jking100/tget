7ce2baafbd891aa253d3f59e338fa88b3cbd4bfb
jking100 <173524547+jking100@users.noreply.github.com>
2024-08-04 18:04:17 -0500

    implemented basic thread pool download workers

--- tget.py
+++ tget.py
@@ -20,11 +20,11 @@ def generate_download_byte_indexes(chunksize: int, totalsize: int, count: int) -
     start = 0
     for i in range(count):
         if(i == 0):
-            index_for_download.append([start,start+chunksize])
+            index_for_download.append((start,start+chunksize))
         else:
-            index_for_download.append([start+1,start+chunksize])
+            index_for_download.append((start+1,start+chunksize))
         start += chunksize
-    index_for_download[-1][1] = totalsize
+    index_for_download[-1] = (index_for_download[-1][0], int(totalsize))
     return index_for_download
 
 def download(url: str, start: int, end: int) -> requests.request: #worker that takes url and start and end byte and downloads the content hten returns its data
@@ -73,30 +73,59 @@ def main() -> None:
     #md5sum of this: b3215c06647bc550406a9c8ccc378756
     args.URL = 'http://ipv4.download.thinkbroadband.com/5MB.zip'
 
-    chunksize = 512 * 1000 * 2
-
     header = requests.head(args.URL)
     if(header.headers['content-length']):
         print(f"Length (Byte): {header.headers['content-length']}")
+    
+    chunksize = int(header.headers['content-length'])
+    chunksize = int(header.headers['content-length'])/1
+    
     count = ceil(int(header.headers['content-length']) / chunksize)
 
-    index_for_download = generate_download_byte_indexes(chunksize, header.headers['content-length'], count)
+    index_for_download = generate_download_byte_indexes(int(chunksize), header.headers['content-length'], count)
 
     bits = b''
 
-    map_list = []
-    for i in range(len(index_for_download)):
-        map_list.append([args.URL, index_for_download[i][0], index_for_download[i][1]])
+    if(False):
+        map_list = []
+        for i in range(len(index_for_download)):
+            map_list.append([args.URL, index_for_download[i][0], index_for_download[i][1]])
 
-    for i in map_list:
+    for i in index_for_download:
         print(i)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        executor.map(download, [args.URL for i in range(count)], [index_for_download[i][0] for i in range(count)], [index_for_download[i][1] for i in range(count)])
+    data = [b''] * count
 
-    print("end")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        future_to_download = {executor.submit(download, args.URL, start, end): (start, end) for start,end in index_for_download}
+        for future in concurrent.futures.as_completed(future_to_download):
+            url = future_to_download[future]
+            #print(url)
 
-    return None
+    if(False):
+        for i in range(count):
+            t = executor.submit(download, args.URL, 
+                         index_for_download[i][0], 
+                         index_for_download[i][1])
+            if t.result().status_code not in [200,206]:
+                print(f"Bad download {args.URL} {index_for_download[i][0]} {index_for_download[i][1]}")
+                return None
+            data[i] = t.result().content
+
+    with open("./testing/parts.dwnl", "wb") as f:
+        for i in range(count):
+            f.write(data[i])
+        print("File wrote")
+    
+            
+
+    if (args.stats and start_time1):
+        elapsed_time = time.perf_counter() - start_time1
+        print(f"\t       Time: {elapsed_time:.2f}s")
+        #print(f"\tDownloading: {download_time:.2f}s ({(download_time/count):.2f}s/chunk)")
+        #print(f"\t    Writing: {filewrite_time:.2f}s")
+
+    return None   
 
     download_time = 0
     filewrite_time = 0
