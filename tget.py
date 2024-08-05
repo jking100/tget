ba47b4617bb26b92c479af00c7e3ea9dede63043
jking100 <173524547+jking100@users.noreply.github.com>
2024-08-05 15:11:54 -0500

    experimenting with changes to how the download is partioned - (removed chunks of a fixed size)

--- tget.py
+++ tget.py
@@ -12,10 +12,12 @@ import random
 import threading
 import concurrent.futures
 from math import ceil
+import re
 
 TGET_VER = "00" + "." + "00" + "." + "02"
 
-def generate_download_byte_indexes(chunksize: int, totalsize: int, count: int) -> list:
+def generate_download_byte_indexes(chunksize: int, totalsize: int) -> list:
+    count = ceil(int(totalsize) / float(chunksize))
     index_for_download = []
     start = 0
     for i in range(count):
@@ -28,7 +30,7 @@ def generate_download_byte_indexes(chunksize: int, totalsize: int, count: int) -
     return index_for_download
 
 def download(url: str, start: int, end: int, id: int) -> requests.request: #worker that takes url and start and end byte and downloads the content hten returns its data
-    print(f"In download {start}")
+    print(f"{id} starting")
     headers = {"Range": f"bytes={start}-{end}"}
     response = requests.get(url, headers=headers)
     
@@ -46,7 +48,7 @@ def get_args():
                                 epilog='Joseph King, 2024'
                                 )
     parser.add_argument("URL", type=str, help="URL of resource to get")
-    parser.add_argument('-t', dest='threads', default=1, type=int, help="Set number of download threads")
+    parser.add_argument('-t', dest='threads', default=8, type=int, help="Set number of download threads (1-16) [default: 8]")
     parser.add_argument('-o', dest='output', default=".", metavar='OUTPUT FILE', type=str, help="Set location of download")
     parser.add_argument('-s', '--stats', action='store_true', help="Toggle download statistic output")
     parser.add_argument('-p', '--python', action='store_true', help="Toggle python info output")
@@ -68,68 +70,69 @@ def main() -> int:
     if (args.stats):
         start_time1 = time.perf_counter()
     
-    #md5sum of this: b3215c06647bc550406a9c8ccc378756
-    args.URL = 'http://ipv4.download.thinkbroadband.com/5MB.zip'
+    args.URL = 'http://ipv4.download.thinkbroadband.com/5MB.zip' # b3215c06647bc550406a9c8ccc378756
+    args.URL = 'http://ipv4.download.thinkbroadband.com/50MB.zip' # 2699c63cb6699b2272f78989b09e88b1
+    regex = '[^\/\\&\?]+\.\w{3,4}(?=([\?&].*$|$))'
+    x = re.search(regex,args.URL)
+    if(x):
+        args.output = f'./{x.group(0)}'
+    else:
+        print("Failed to grab file name from url, defaulting to tget-XX.dwnl")
+        args.output = f'./tget-{random.randint(0,9)}{random.randint(0,9)}.dwnl'
 
+    #check if output location is valid
+    try:
+        with open(args.output, 'wb') as file:
+            pass
+    except IOError as e:
+        print("Error with output file location.")
+        print(f"Error: {e}")
+        return -1
+    
+    print("File: ",args.output)
     header = requests.head(args.URL)
-    if (header.status_code != (200|206)):
+    if (header.status_code not in (200, 206)):
         print(f"Error: {header.status_code}\n \
               URL: {args.URL}")
         return -1
         
-    if(header.headers['content-length']):
-        print(f"Length (Byte): {header.headers['content-length']}")
-    
-    chunksize = 512 * 1000
-    #chunksize = int(header.headers['content-length'])/5
-    
-    count = ceil(int(header.headers['content-length']) / chunksize)
+    if(header.headers['content-length'] and args.stats):
+        print(f"Size (Byte): {header.headers['content-length']} B")
+        print(f"Size (MByte): {int(int(header.headers['content-length'])/float(1024*1024))} MB")
 
-    index_for_download = generate_download_byte_indexes(int(chunksize), header.headers['content-length'], count)
+    if(args.threads not in range(1,17)):
+        print("Error: Threads must be a value between (1-16)")
+        return -1
+    
+    chunksize = int(int(header.headers['content-length']) / float(args.threads))
+    
+    
+    chunkcount = args.threads
+
+    work_list = generate_download_byte_indexes(int(chunksize), header.headers['content-length'])
 
     bits = b''
 
-    if(False):
-        map_list = []
-        for i in range(len(index_for_download)):
-            map_list.append([args.URL, index_for_download[i][0], index_for_download[i][1]])
+    if(args.stats):
+        for i in work_list:
+            print(f"{i[1]-i[0]} - {i}")
 
-    for i in index_for_download:
-        print(f"{i[1]-i[0]} - {i}")
+    data = [b''] * chunkcount
 
-    data = [b''] * count
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=min(count, 24)) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=min(args.threads, 16)) as executor:
         future_to_download = {
-            executor.submit(download, args.URL, start, end, id): (start, end, id) for start, end, id in index_for_download
+            executor.submit(download, args.URL, start, end, id): (start, end, id) for start, end, id in work_list
             }
         
         for future in concurrent.futures.as_completed(future_to_download):
-
             _, _, id = future_to_download[future]
-            #print(start)
-            print(id)
-
             data[id] = (future.result().content)
-            #print(url)
 
-    if(False):
-        for i in range(count):
-            t = executor.submit(download, args.URL, 
-                         index_for_download[i][0], 
-                         index_for_download[i][1])
-            if t.result().status_code not in [200,206]:
-                print(f"Bad download {args.URL} {index_for_download[i][0]} {index_for_download[i][1]}")
-                return None
-            data[i] = t.result().content
-
-    with open("./testing/parts.dwnl", "wb") as f:
-        for i in range(count):
+    with open(f"./testing/{args.output}", "wb") as f:
+        for i in range(chunkcount):
             f.write(data[i])
         print("File wrote")
     
-            
-
     if (args.stats and start_time1):
         elapsed_time = time.perf_counter() - start_time1
         print(f"\t       Time: {elapsed_time:.2f}s")
@@ -137,35 +140,6 @@ def main() -> int:
         #print(f"\t    Writing: {filewrite_time:.2f}s")
 
     return 1   
-
-    download_time = 0
-    filewrite_time = 0
-
-    for i in index_for_download:
-        start_time = time.perf_counter()
-        response = download(args.URL, i[0], i[1])
-        elapsed_time = time.perf_counter() - start_time
-        download_time += elapsed_time
-        #print(f"\tTime: {elapsed_time:.2f}s")
-        start_time = time.perf_counter()
-        bits += response.content
-        elapsed_time = time.perf_counter() - start_time
-        filewrite_time += elapsed_time
-    
-    with open("./testing/parts.dwnl", "wb") as f:
-        start_time = time.perf_counter()
-        f.write(bits)
-        elapsed_time = time.perf_counter() - start_time
-        filewrite_time += elapsed_time
-        print("File wrote")
-
-    if (args.stats and start_time):
-        elapsed_time = time.perf_counter() - start_time1
-        print(f"\t       Time: {elapsed_time:.2f}s")
-        print(f"\tDownloading: {download_time:.2f}s ({(download_time/count):.2f}s/chunk)")
-        print(f"\t    Writing: {filewrite_time:.2f}s")
-
-    return None    
 
 if __name__ == "__main__":
     main()
