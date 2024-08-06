f21e24fa1078256e64b722760fb48d41aca9af05
jking100 <173524547+jking100@users.noreply.github.com>
2024-08-06 03:50:09 -0500

    big commit :( - refactored to now utilize a worker object to be created for the threadpool. file writing can now happened in any order, no need to wait for all threads to finish download

--- tget.py
+++ tget.py
@@ -14,7 +14,7 @@ import concurrent.futures
 from math import ceil
 import re
 
-TGET_VER = "00" + "." + "00" + "." + "02"
+TGET_VER = "00" + "." + "00" + "." + "03"
 
 def generate_download_byte_indexes(chunksize: int, totalsize: int) -> list:
     count = ceil(int(totalsize) / float(chunksize))
@@ -48,27 +48,51 @@ def get_args():
                                 epilog='Joseph King, 2024'
                                 )
     parser.add_argument("URL", type=str, help="URL of resource to get")
-    parser.add_argument('-t', dest='threads', default=8, type=int, help="Set number of download threads (1-16) [default: 8]")
+    parser.add_argument('-t', dest='threads', default=4, type=int, help="Set number of download threads (1-10) [default: 4]")
     parser.add_argument('-o', dest='output', default=".", metavar='OUTPUT FILE', type=str, help="Set location of download")
-    parser.add_argument('-s', '--stats', action='store_true', help="Toggle download statistic output")
-    parser.add_argument('-p', '--python', action='store_true', help="Toggle python info output")
+    parser.add_argument('--md5', dest='md5', default=None, type=str, help="Check download against source md5 checksum")
     parser.add_argument('--version', action='version', version=f"Tget version: {TGET_VER}")
     
     return parser.parse_args()
 
+class thread_worker: #an object that knows only how to open http connection and then write those bytes into a given file object, intended to be called as a subthread
+    
+    def __init__(self, url: str, start: int, end: int, thread_id: int, thread_lock, file):
+        self.url = url
+        self.start = start
+        self.end = end
+        self.thread_id = thread_id
+        self.thread_lock = thread_lock
+        self.file = file
+        self.content = b'' * (end-start)
+
+    def execute(self) -> int:
+        headers = {"Range": f"bytes={self.start}-{self.end}"}
+        response = requests.get(self.url, headers=headers)
+
+        if(response.status_code not in [200,206]):
+            return response.status_code
+                
+        with self.thread_lock:
+            self.file.seek(self.start)
+            self.file.write(response.content)
+
+        return response.status_code
+
+    def get_content(self):
+        return self.content
+
+def execute_thread_workers(url, start, end, thread_id, thread_lock, file):
+    #url, start, end, thread_id, thread_lock, file = zip(*worker_instructions)
+
+    print("Starting: ", thread_id)
+    worker = thread_worker(url, start, end, thread_id, thread_lock, file)
+    return worker.execute()
+
 def main() -> int:
     args = get_args()        
-
-    if(args.stats or args.python):
-        print("-" * 80)
-        if(args.python):
-            print(f"Python env.: {sys.executable} ({sys.version.split()[0]})")
-        if(args.stats):
-            print(str(args))
-        print("-" * 80)
     
-    if (args.stats):
-        start_time1 = time.perf_counter()
+    start_time_main = time.perf_counter()
     
     args.URL = 'http://ipv4.download.thinkbroadband.com/5MB.zip' # b3215c06647bc550406a9c8ccc378756
     args.URL = 'http://ipv4.download.thinkbroadband.com/50MB.zip' # 2699c63cb6699b2272f78989b09e88b1
@@ -96,15 +120,15 @@ def main() -> int:
               URL: {args.URL}")
         return -1
         
-    if(header.headers['content-length'] and args.stats):
+    if(header.headers['content-length']):
         print(f"Size (Byte): {header.headers['content-length']} B")
         print(f"Size (MByte): {int(int(header.headers['content-length'])/float(1024*1024))} MB")
 
-    if(args.threads not in range(1,17)):
-        print("Error: Threads must be a value between (1-16)")
+    if(args.threads not in range(1,11)):
+        print("Error: Threads must be a value between (1-10)")
         return -1
     
-    chunksize = int(int(header.headers['content-length']) / float(args.threads))
+    chunksize = ceil(int(header.headers['content-length']) / float(args.threads))
     
     
     chunkcount = args.threads
@@ -113,29 +137,48 @@ def main() -> int:
 
     bits = b''
 
-    if(args.stats):
-        for i in work_list:
-            print(f"{i[1]-i[0]} - {i}")
+
+    for i in work_list:
+        print(f"{i} [{i[1]-i[0]}]B")
 
     data = [b''] * chunkcount
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=min(args.threads, 16)) as executor:
-        future_to_download = {
-            executor.submit(download, args.URL, start, end, id): (start, end, id) for start, end, id in work_list
-            }
-        
-        for future in concurrent.futures.as_completed(future_to_download):
-            _, _, id = future_to_download[future]
-            data[id] = (future.result().content)
+    with open(f"./testing/{args.output}", "wb") as file:
+        #for file writing with mutliple threads targeting same file
+        thread_lock = threading.Lock()
 
-    with open(f"./testing/{args.output}", "wb") as f:
-        for i in range(chunkcount):
-            f.write(data[i])
-        print("File wrote")
+        worker_instructions = []
+        for i in range(args.threads):
+            #url, start, end, thread_id, thread_lock, file
+            worker_instructions.append(
+                                        (args.URL, 
+                                        work_list[i][0], #start
+                                        work_list[i][1], #end
+                                        work_list[i][2], #id
+                                        thread_lock,
+                                        file)                        
+            )
+        #for i in worker_instructions:
+        #    print(i)
+        
+    #init
+    #open file object
+        with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
+            task_dict = { 
+                executor.submit(
+                    execute_thread_workers, 
+                    args.URL, start, end, thread_id, thread_lock, file
+                    ): (start, end, thread_id) 
+                    for _, start, end, thread_id, _, _ in worker_instructions
+            }            
+            
+            for task in concurrent.futures.as_completed(task_dict):
+                _, _, id = task_dict[task]
+                if task.done:
+                    print(f"terminate: {id} - {task.result()}")
     
-    if (args.stats and start_time1):
-        elapsed_time = time.perf_counter() - start_time1
-        print(f"\t       Time: {elapsed_time:.2f}s")
+    elapsed_time_main = time.perf_counter() - start_time_main
+    print(f"\tGlobal Time: {elapsed_time_main:.2f}s")
         #print(f"\tDownloading: {download_time:.2f}s ({(download_time/count):.2f}s/chunk)")
         #print(f"\t    Writing: {filewrite_time:.2f}s")
 
@@ -143,3 +186,4 @@ def main() -> int:
 
 if __name__ == "__main__":
     main()
+    import resource; print(f"Max Mem Usage (MB): {int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024)}")
