9adb066b709bfc8cbf3ca603177c47f8d6bc53a0
jking100 <173524547+jking100@users.noreply.github.com>
2024-08-18 14:23:48 -0500

    created a class to managing running the threads and collecting data back from them

--- tget.py
+++ tget.py
@@ -26,8 +26,8 @@
 
 import argparse
 import requests
-from time import perf_counter
-from random import randint
+import time
+import random
 import threading
 import concurrent.futures
 from math import ceil
@@ -38,33 +38,160 @@ import hashlib
 
 TGET_VER = "01" + "." + "01"
 
-def generate_thread_byte_indexes(threads: int, totalsize: int) -> list:
-    byte_portion = ceil(totalsize / float(threads))
-    count = ceil(totalsize / float(byte_portion))
-    thread_byte_index = []
-    start = 0
-    for i in range(count):
-        if(i == 0):
-            thread_byte_index.append((i, start, start+byte_portion))
-        else:
-            thread_byte_index.append((i, start+1, start+byte_portion))
-        start += byte_portion
-    thread_byte_index[-1] = (count-1, thread_byte_index[-1][1], totalsize)
-    #for i in thread_byte_index: print(i)
-    return thread_byte_index
-
 def get_args():
     parser = argparse.ArgumentParser(
                                 prog='tget',
                                 description='A parallel download acceleration utility',
                                 )
-    parser.add_argument("URL", type=str, help="URL of resource to get")
-    parser.add_argument('-t', dest='threads', metavar='{1-10}', default=4, type=int, help="Set number of download threads (default: 4)")
+    parser.add_argument("URL", type=str, help="")
+    parser.add_argument('-t', dest='threads', metavar='[1-8]', default=4, type=int, choices=range(1,9), help="Set number of download threads (default: 4)")
     parser.add_argument('-o', dest='output', default=None, metavar='FILE', type=str, help="write download to FILE")
-    parser.add_argument('--checksum', dest='sha', metavar='', default=None, type=str, help="Check download against given sha256 checksum")
+    parser.add_argument('--sha256', dest='sha', metavar='', default=None, type=str, help="Check download against given sha256 checksum")
     parser.add_argument('--version', action='version', version=f"Tget v{TGET_VER} - A parallel download acceleration utility (Joseph King, 2024)")
     
     return parser.parse_args()
+
+def validate_input(args): #checks all arguments for validity
+    # return Tuple("Error String", Boolean(Abort_Main), HttpResponseheader)
+    #check url, threads, output, and sha (64 char long)
+
+    ##################################################################
+    #Check URL
+    print(f"Checking connection to `{args.URL}`")
+    header = requests.head(args.URL)
+    if (header.status_code not in (200, 206)):
+        return (f"Connection Failure: {header.status_code}", True)
+    if (not header.headers['content-length'] or not header.headers['content-type']):
+        return (f"Resource Failure: Failed to retrieve necessary file info", True)
+    print(" "*2 + f"Connection ok {header.status_code}")
+
+    if (int(header.headers['content-length']) < (1024**2)):
+        print(" "*2 + f"Size: {int(header.headers['content-length'])/1024:.0f}KB ", end='')
+    elif (int(header.headers['content-length']) < (1024**3)):
+        print(f"{int(header.headers['content-length'])/(1024**2):.0f}MB ", end='')
+    else:
+        print(f"{int(header.headers['content-length'])/(1024**3):.0f}GB ", end='')
+    print(f"[{header.headers['content-type']}]")
+
+    ##################################################################
+    #threads are checked by argparse object in its definition
+
+    ##################################################################
+    #Check output file path if given, else default to basic from url
+
+    final_file_path = None
+    if (args.output): #user specified FILE path
+        final_file_path = Path(args.output)
+    else: #no user specification
+        #regex the url to get the file name
+        url_parts = args.URL.split("/")
+        file_name = url_parts[-1]
+        if(not match(r"^([\w\.-]+)", file_name)):
+            print("Warning: Failed to retrieve filename, defaulting to tget-{date}.dwnl")
+            date = header.headers["Date"].split()[2] + header.headers["Date"].split()[1]
+            file_name = f'tget-{date}.dwnl'
+
+        final_file_path = Path(Path().resolve(), Path(file_name)) #should be windows compatible
+    
+    #overwrite args.output with final_file_path so args.output is now String -> Path()
+    args.output = final_file_path
+    #print("-"*80 + f"\nDEBUG: {args.output}\n" + "-"*80)
+
+    #check if file can be opened
+    print(f"Testing file output to `{args.output}`")
+    try:
+        with open(args.output, 'wb') as file:
+            pass
+    except IOError as e:
+        return (f"Error: {str(e)[10:]}", True)
+    
+    print(" "*2 + "File output okay")
+
+    ##################################################################
+    #Check SHA is 64 Chars
+    if(args.sha):
+        if (len(args.sha) != 64):
+            args.sha = "INVALID"
+            print("WARN: Invalid checksum, disabling verification")
+
+    return (f"\nInitializing download | {args.threads} threads", False, header)
+
+class stopwatch:
+    def __init__(self):
+        self.start_time = time.perf_counter()
+
+    def time_elapsed(self):
+        return time.perf_counter() - self.start_time
+    
+class thread_manager:
+    def __init__(self, header, file_path, url, threads):
+        self.workers: thread_worker = []
+        self.file_path = file_path
+        self.url = url
+        self.threads = threads
+        self.file_size = int(header.headers['content-length'])
+        self.results = []
+
+    def _generate_thread_byte_indexes(self, threads: int, totalsize: int) -> list:
+
+        byte_portion = ceil(totalsize / float(threads))
+        count = ceil(totalsize / float(byte_portion))
+        thread_byte_index = []
+        start = 0
+        for i in range(count):
+            if(i == 0):
+                thread_byte_index.append((i, start, start+byte_portion))
+            else:
+                thread_byte_index.append((i, start+1, start+byte_portion))
+            start += byte_portion
+        thread_byte_index[-1] = (count-1, thread_byte_index[-1][1], totalsize)
+        #for i in thread_byte_index: print(i)
+        return thread_byte_index
+
+    def _execute_thread_workers(self, url, start, end, thread_id, thread_lock, file, stop_work):
+        worker = thread_worker(url, start, end, thread_id, thread_lock, file, stop_work)
+        return worker.execute()
+    
+    def start_download(self):
+        #init connection
+
+        try:
+            with open(self.file_path, "wb") as file:
+                #for file writing with mutliple threads targeting same file
+                thread_lock = threading.Lock()
+                stop_work = threading.Event()
+                is_download_ok = True
+                
+                thread_byte_index = self._generate_thread_byte_indexes(self.threads, self.file_size)
+
+                with concurrent.futures.ThreadPoolExecutor(max_workers=self.threads) as executor:
+                    print(f"\nDownloading ({self.threads} threads)")
+                    task_dict = { 
+                        executor.submit(
+                            self._execute_thread_workers, 
+                            self.url, start, end, thread_id, thread_lock, file, stop_work
+                            ): (thread_id, start, end) 
+                            for thread_id, start, end in thread_byte_index
+                    }            
+
+                    for task in concurrent.futures.as_completed(task_dict):
+                        id, _, _ = task_dict[task]
+                        if task.result() not in (200, 206):
+                            stop_work.set()
+                            print(f"\nDownload Failed: Connection error [{task.result()}]")
+                            with thread_lock:
+                                print("Deleting")
+                                Path.unlink(self.file_path, missing_ok=True)
+                            is_download_ok = False
+                            break
+
+                if not is_download_ok:
+                    return -1
+                return 0
+                
+        except Exception as e:
+            print(f"thread_manager::start_download(): {e}")
+            return -1
 
 class thread_worker: #an object that knows only how to open http connection and then write those bytes into a given file object, intended to be called as a subthread
     
@@ -85,12 +212,12 @@ class thread_worker: #an object that knows only how to open http connection and 
         #if(self.thread_id == 2): response.status_code = 201
         
         if(response.status_code not in [200,206]):
-            print(f"\tt{self.thread_id} {response.status_code} fail")
+            print(f"  t{self.thread_id} {response.status_code} fail")
             return response.status_code
-        print(f"    t{self.thread_id} {response.status_code} ok")
+        print(f"  t{self.thread_id} {response.status_code} ok")
                 
-        #1mb seems to be pretty good comparing file ops/mem use. None=unlimit http repsonse size (MAX MEM usually)
-        for i in response.iter_content(chunk_size=1024*1024, decode_unicode=False):
+        #1mb seems to be pretty good
+        for i in response.iter_content(chunk_size=1024**2, decode_unicode=False):
             if self.stop_work.is_set(): return -1
             with self.thread_lock:
                 self.file.seek(self.start+self.cursor_offset)
@@ -99,135 +226,45 @@ class thread_worker: #an object that knows only how to open http connection and 
 
         return response.status_code
 
-def execute_thread_workers(url, start, end, thread_id, thread_lock, file, stop_work, path):
-    worker = thread_worker(url, start, end, thread_id, thread_lock, file, stop_work)
-    return worker.execute()
-
-def check_sha256(file, checksum: str) -> bool:
-    #2dce83fbc71ddffac53b37c484088fa2334f39641fa0d9ca6516fd0f656dac2a
-    digest = hashlib.file_digest(file, "sha256")
-    file_checksum = digest.hexdigest()
-
-    return True if (file_checksum == checksum) else False
-
-class stopwatch:
-    #Honestly (probably) needless wasteful usage of OOP, but lets try seems easier to implement across various components
-    def __init__(self):
-        self.start_time = perf_counter()
-
-    def time_elapsed(self):
-        return perf_counter() - self.start_time
-    
 def main() -> int:
-    args = get_args()        
+    args = get_args()  
+
+    validation_msg, is_error, header = validate_input(args)
+    print(validation_msg)
+    if (is_error):
+        return -1
     
     overall_timer = stopwatch()
 
-    #These blocks should probably be functioned off since they are basically
-    #   just input validation and checking the web for responses
-    ##################################################################
-    if(args.threads not in range(1,11)):
-        print("Error: Threads must be a value between (1-10)")
-        return -1
-    ##################################################################
-    #if -O : use has passed a valid file (abs, or relative path) with their own name. write it
-    #if !-O: default to regex of file name and download to working directory
-    url_parts = args.URL.split("/")
-    name = url_parts[-1]
-    regex = r"^([\w\.-]+)"
-    x = match(regex,name)
-    if(not x):
-        print("Warn: Failed to parse filename, defaulting to tget-{0-9}.dwnl")
-        name = f'./tget-{randint(0,9)}.dwnl'
-    ##################################################################
-    #check if output location is valid
-    pwd = Path().resolve()
-    download_file_path = Path(pwd+name) if (args.output == None) else Path(args.output)
-
-    #print("-"*80 + f"\nDEBUG: {download_file_path}\n" + "-"*80)
-    try:
-        with open(args.output, 'wb') as file:
-            pass
-    except IOError as e:
-        print(f"Error: Failed to initialize file path `{args.output}`")
-        print(f"\t{e}")
-        return -1
-    
-    ##################################################################
-    
-    ##################################################################
-    print("Checking connection to", args.URL)
-    header = requests.head(args.URL)
-    if (header.status_code not in (200, 206)):
-        print(f"Error: {header.status_code}")
-        return -1
-    if (not header.headers['content-length'] or not header.headers['content-type']):
-        print("Error with HTTP response: Failed to retreive file info")
-        return -1
-    ##################################################################
-    
-    print(f"Connection ok {header.status_code}")  
-    print(f"Size: {int(header.headers['content-length'])/1024:.0f}KB ", end='')
-    print(f"{int(header.headers['content-length'])/(1024*1024):.0f}MB ", end='')
-    print(f"[{header.headers['content-type']}]")
-    print(f"Saving to: '{args.output}'")
-
     ############################################################################
+    #input is good, call the manager to spool up the threads
+    #manager will handle returning the info of the download
+    manager = thread_manager(header, args.output, args.URL, args.threads)
+    manager.start_download()
 
-    try:
-        with open(args.output, "wb") as file:
-            #for file writing with mutliple threads targeting same file
-            thread_lock = threading.Lock()
-            stop_work = threading.Event()
-            is_download_ok = True
-            
-            thread_byte_index = generate_thread_byte_indexes(args.threads, int(header.headers['content-length']))
 
-            with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
-                print(f"\nDownloading ({args.threads} threads)")
-                task_dict = { 
-                    executor.submit(
-                        execute_thread_workers, 
-                        args.URL, start, end, thread_id, thread_lock, file, stop_work
-                        ): (thread_id, start, end) 
-                        for thread_id, start, end in thread_byte_index
-                }            
+    total_time = overall_timer.time_elapsed()
+    print(f"\nDownload Complete - `{args.output}` in {total_time:.1f}s @ {int(header.headers['content-length'])/float(1024*1024*total_time):.1f}MB/s")
 
-                for task in concurrent.futures.as_completed(task_dict):
-                    id, _, _ = task_dict[task]
-                    if task.result() not in (200, 206):
-                        stop_work.set()
-                        print(f"\nDownload Failed - `{args.output}` Connection error [{task.result()}]")
-                        with thread_lock:
-                            print("Deleting")
-                            Path.unlink(args.output, missing_ok=True)
-                        is_download_ok = False
-                        break
 
-            if not is_download_ok:
+    ########################
+    if(args.sha != None):
+            try:
+                with open(args.output, "rb") as file:
+                    file_checksum = hashlib.file_digest(file, "sha256").hexdigest()
+                    print(f"Checksum `{args.output}`: {file_checksum}")
+                    if(args.sha != "INVALID"):
+                        if(file_checksum != args.sha):
+                            print(f"Checksum Verification - fail")
+                        else:
+                            print(f"Checksum Verification - pass")
+
+            except Exception as e:
+                print("Checksum verify:")
+                print(f"File Error: {e}")
                 return -1
-            total_time = overall_timer.time_elapsed()
-            print(f"\nDownload Complete - `{args.output}` in {total_time:.1f}s @ {int(header.headers['content-length'])/float(1024*1024*total_time):.1f}MB/s")
-
-    except Exception as e:
-        print(f"File Error: {e}")
-        return -1
-    
-    if(args.sha):
-        try:
-            with open(args.output, "rb") as file:
-                if(not check_sha256(file, args.sha)):
-                    print(f"Checksum verification - fail")
-                else:
-                    print(f"Checksum verification - pass")
-
-        except Exception as e:
-            print("Checksum verify:")
-            print(f"File Error: {e}")
-            return -1
     
     return 0
-
 
 if __name__ == "__main__":
     main()
